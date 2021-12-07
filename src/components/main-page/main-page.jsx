fbee889c29928f32408b588939aba8e909e04d40
Alexandra <alexkuchynsk@gmail.com>
2021-12-07 14:21:27 +0100

    Fixed guitar background

--- src/components/main-page/main-page.jsx
+++ src/components/main-page/main-page.jsx
@@ -3,12 +3,19 @@ import './main.scss';
 import Catalog from '../catalog/catalog.jsx';
 import Header from '../header/header';
 import Footer from '../footer/footer';
+// import {ReactComponent as LineHeader} from '../../img/line-header.svg';
 
 const MainPage = () => {
+//   const linesHeaderArray = [1, 2, 3, 4, 5, 6];
   return <>
     <Header isMainPage={true}/>
     <main className="main main--catalog">
       <h1 className="visually-hidden">Your Guitar Shop</h1>
+      {/* <div className="main__lines-header">
+        {linesHeaderArray.map((line) => {
+          return <LineHeader key={line}/>;
+        })}
+      </div> */}
       <Catalog/>
     </main>
     <Footer isMainPage={true}/>
